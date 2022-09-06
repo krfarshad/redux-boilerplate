235fc564046bab7e34e0a81811c3e71e556f8445
Farshad <krfarshad@gmail.com>
2022-09-06 10:23:38 +0430

    refactor : add combineReducers

---
+++ src/redux/mergeReducers.js
@@ -0,0 +1,28 @@
+import { combineReducers } from "redux";
+
+const counterReducer = (state = 0, action) => {
+    switch(action.type){
+        case 'INCREMENT'  :
+            return state + action.payload
+        case 'DECREMENT':
+            return state - 1
+        default:
+            return state
+    }
+}
+
+const loggedReducer = (state=false, action) => {
+    switch(action.type){
+        case 'SIGN_IN':
+            return !state;
+        default:
+            return state;
+    }
+}
+
+const allReducers = combineReducers({
+    counter : counterReducer ,
+    isLogged :loggedReducer
+})
+
+export default allReducers;
